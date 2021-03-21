153b61614d8605c9484cfd4c88c830a9bd5a3df6
Gustavo Rodriguez <gustavguez@gmail.com>
2021-03-21 15:43:43 -0300

    feat(select): remove disable attribute

--- projects/gustavguez/ngx-core/src/lib/forms/ngx-gustavguez-select/ngx-gustavguez-select.component.ts
+++ projects/gustavguez/ngx-core/src/lib/forms/ngx-gustavguez-select/ngx-gustavguez-select.component.ts
@@ -10,7 +10,6 @@ export class NgxGustavguezSelectComponent {
 	// Inputs
 	@Input() form: FormGroup;
 	@Input() label: string;
-	@Input() disabled: boolean;
 	@Input() controlId: string;
 	@Input() controlName: string;
 	@Input() requiredErrorText: string;
